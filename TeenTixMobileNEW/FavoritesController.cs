76aefc99fc3122932017868974248bb60e30399f
Laura Valiente <lauramvaliente@gmail.com>
2016-02-28 18:42:12 -0800

    navigation bar for pages as tabs

---
+++ TeenTixMobileNEW/FavoritesController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace TeenTixMobileNEW
+{
+	partial class FavoritesController : UIViewController
+	{
+		public FavoritesController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
---
+++ TeenTixMobileNEW/HomeController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace TeenTixMobileNEW
+{
+	partial class HomeController : UIViewController
+	{
+		public HomeController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
